0cb8c563fa1107290258959c0a7dead8328e8f5b
Jorge <jorgelopezsarry@gmail.com>
2022-02-21 09:07:12 +0100

    newtest

--- strapi-seo-panel/config/admin.js
+++ strapi-seo-panel/config/admin.js
@@ -2,4 +2,7 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', '82a40b1a3beeb110ecb89bc48263b2b8'),
   },
+  watchIgnoreFiles: [
+    '**/config/sync/**',
+  ],
 });
