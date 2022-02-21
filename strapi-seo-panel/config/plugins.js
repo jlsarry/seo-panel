b921eb9eda2d49be5f97d2cfddb289ea06c14491
Jorge <jorgelopezsarry@gmail.com>
2022-02-21 08:21:15 +0100

    changes

---
+++ strapi-seo-panel/config/plugins.js
@@ -0,0 +1,34 @@
+module.exports = ({ env }) => ({
+	slugify: {
+		enabled: true,
+		config: {
+			contentTypes: {
+				category: {
+					field: 'slug',
+					references: 'name',
+				},
+                brand: {
+                    field: 'slug',
+                    references: 'name',
+                },
+                print_provider: {
+					field: 'slug',
+					references: 'name',
+				},
+                subcategory: {
+                    field: 'slug',
+                    references: 'name',
+                },
+                product: {
+                    field: 'slug',
+                    references: 'name',
+                },
+			},
+            slugifyOptions: {
+                customReplacements: [
+                    ['&', 'and'],
+                ]
+            },
+		},
+	},
+});
